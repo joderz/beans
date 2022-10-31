2c896d3f4f74939c3e0f9320dcb151a58a571533
joderz <66087885+joderz@users.noreply.github.com>
2022-10-31 16:11:49 +0800

    Update models.py

--- models.py
+++ models.py
@@ -89,6 +89,159 @@ class Modelz:
                                     """)
 
 
+    def host_1(self):
+        return self.executeRawSql("""select h.host_name,h.email,h.account_created
+                                    from host h
+                                    where h.review_scores_rating=100
+                                    order by h.number_of_reviews;
+                                    """)
+
+    def host_2(self):
+        return self.executeRawSql("""select distinct h1.host_name, h1.email, h1.account_created, h1.number_of_reviews
+                                    from host h1
+                                    where h1.number_of_reviews>=all(
+                                    select h2.number_of_reviews
+                                    from host h2
+                                    where h1.email!=h2.email)
+                                    group by h1.email;
+                                    """)
+
+    def host_3(self):
+        return self.executeRawSql("""select h.email, h.review_scores_cleanliness, h.review_scores_accuracy, h.review_scores_communication, h.review_scores_location, h.review_scores_checkin, h.review_scores_value
+                                    from host h
+                                    where h.review_scores_cleanliness<=5 
+                                    or h.review_scores_accuracy<=5
+                                    or h.review_scores_communication<=5
+                                    or h.review_scores_location<=5
+                                    or h.review_scores_checkin<=5
+                                    or h.review_scores_value<=5;
+                                    """)   
+
+    def host_4(self):
+        return self.executeRawSql("""select h.host_name,h.email,h.number_of_listings
+                                    from host h
+                                    order by h.number_of_listings desc;
+                                    """)
+
+    def host_5(self):
+        return self.executeRawSql("""select h.email, h.number_of_listings, p.country, p.city
+                                    from host h, property p
+                                    where h.email=p.email
+                                    group by h.email, p.country, p.city
+                                    order by h.number_of_listings desc;
+                                    """)
+
+    def host_6(self):
+        return self.executeRawSql("""select h.email, sum(r.duration*r.price_per_day) as total_spending
+                                    from reservation r, host h
+                                    where r.host_email=h.email
+                                    group by h.email
+                                    order by sum(r.duration*r.price_per_day) desc;
+                                    """)    
+
+    def property_11(self):
+        return self.executeRawSql("""select p.property_type, trunc(avg(h.review_scores_rating),0) as avg_rating
+                                    from property p, host h
+                                    where p.email=h.email 
+                                    and h.review_scores_rating is not null
+                                    group by p.property_type
+                                    order by avg(h.review_scores_rating) DESC;
+                                    """)
+
+    def property_12(self):
+        return self.executeRawSql("""select p.room_type, trunc(avg(h.review_scores_rating),0) as avg_rating
+                                    from property p, host h
+                                    where p.email=h.email 
+                                    and h.review_scores_rating is not null
+                                    group by p.room_type
+                                    order by avg(h.review_scores_rating) DESC;
+                                    """)
+
+    def property_13(self):
+        return self.executeRawSql("""select p.accommodates, trunc(avg(h.review_scores_rating),0) as avg_rating
+                                    from property p, host h
+                                    where p.email=h.email 
+                                    and h.review_scores_rating is not null
+                                    group by p.accommodates
+                                    order by avg(h.review_scores_rating) DESC;
+                                    """)   
+
+    def property_14(self):
+        return self.executeRawSql("""select p.city, trunc(avg(h.review_scores_rating),0) as avg_rating
+                                    from property p, host h
+                                    where p.email=h.email 
+                                    and h.review_scores_rating is not null
+                                    group by p.city
+                                    order by avg(h.review_scores_rating) DESC;
+                                    """)
+
+    def property_21(self):
+        return self.executeRawSql("""select sum(r.price_per_day*r.duration) as total_revenue, p.city, d.year
+                                    from reservation r
+                                    natural join property p
+                                    natural join date d
+                                    where d.year='2022'
+                                    group by p.city, d.year
+                                    order by sum(r.price_per_day*r.duration) desc;
+
+                                    """)
+
+    def property_22(self):
+        return self.executeRawSql("""select sum(r.price_per_day*r.duration) as total_revenue, p.city, d.year
+                                    from reservation r
+                                    natural join property p
+                                    natural join date d
+                                    where d.year='2021'
+                                    group by p.city, d.year
+                                    order by sum(r.price_per_day*r.duration) desc;
+
+                                    """) 
+
+    def property_23(self):
+        return self.executeRawSql("""select sum(r.price_per_day*r.duration) as total_revenue, p.city, d.year
+                                    from reservation r
+                                    natural join property p
+                                    natural join date d
+                                    where d.year='2020'
+                                    group by p.city, d.year
+                                    order by sum(r.price_per_day*r.duration) desc;
+
+                                    """)
+
+    def property_31(self):
+        return self.executeRawSql("""select p.property_type, trunc(avg(r.number_of_guests),0)
+                                    from property p, reservation r
+                                    where p.property_id=r.property_id
+                                    group by p.property_type;
+                                    """)
+
+    def property_32(self):
+        return self.executeRawSql("""select p.room_type, trunc(avg(r.number_of_guests),0) as avg_guests
+                                    from property p, reservation r
+                                    where p.property_id=r.property_id
+                                    group by p.room_type;
+                                    """)   
+
+    def property_33(self):
+        return self.executeRawSql("""select p.city, trunc(avg(r.number_of_guests),0) as avg_guests
+                                    from property p, reservation r
+                                    where p.property_id=r.property_id
+                                    group by p.city;
+                                    """)
+
+    def property_41(self):
+        return self.executeRawSql("""select p.property_type, count(p.property_type) as no_of_propertytype , p.country, p.city
+                                    from property p
+                                    group by p.property_type, p.country, p.city
+                                    order by count(p.property_type) desc;
+                                    """)
+
+    def property_42(self):
+        return self.executeRawSql("""select p.room_type, count(p.room_type) as no_of_propertytype , p.country, p.city
+                                    from property p
+                                    group by p.room_type, p.country, p.city
+                                    order by count(p.room_type) desc;
+                                    """) 
 
     def overview(self,value):
         return self.executeRawSql("""SELECT 
@@ -110,6 +263,9 @@ class Modelz:
                                         AND p.beds >= :beds
                                         AND p.bathrooms >= :bathrooms
                                         AND h.review_scores_rating >= :rating;""",value)
+
+
+
 #result = db.session.execute('SELECT * FROM my_table WHERE my_column = :val', {'val': 5})
 
 
